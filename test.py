49cab83b8652a62336633a1437066f7686b6bc1f
CBHY <codebyhy@gmail.com>
2022-03-08 15:04:03 +0900

    Frist commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import scipy
+import mglearn
+import sklearn
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
